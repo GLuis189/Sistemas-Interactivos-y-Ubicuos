f8aa06792e8eaa37b341aead19d19ba36fd63c74
GLuis189 <94182328+GLuis189@users.noreply.github.com>
2024-03-12 17:38:46 +0100

    lab2

--- lab2/index.js
+++ lab2/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const PORT = 80;
+const PORT = 8080;
 
 const serveStaticFile = async (file) => {
   return new Promise((resolve, reject) => {
@@ -37,17 +37,39 @@ const handleRequest = async (request, response) => {
         content = await serveStaticFile("www/style.css");
         contentType = "text/css";
         break;
+      case "/tasks/get":
+        content = await serveStaticFile("tasks.json");
+        contentType = "application/json";
+        break;
       default: 
         content = "Ruta no v&aacutelida\r\n";
         contentType = "text/html";
     }
 
      sendResponse(response, content, contentType);
-  } else{
-     response.writeHead(405, {"Content-Type": "text/html"});
-     response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
+    } else if (request.method === "POST" && url === "/tasks/update") {
+      let body = '';
+      request.on('data', (chunk) => {
+        body += chunk.toString(); // Convertir los datos del cuerpo de la solicitud a cadena
+      });
+      request.on('end', async () => {
+        try {
+          const updatedTasks = JSON.parse(body); // Convertir la cadena JSON a objeto JavaScript
+          await fs.promises.writeFile("tasks.json", JSON.stringify(updatedTasks, null, 2)); // Escribir los datos actualizados en el archivo tasks.json
+          sendResponse(response, "Tasks updated successfully", "text/plain");
+        } catch (error) {
+          console.error("Error updating tasks:", error);
+          response.writeHead(500, {"Content-Type": "text/html"});
+          response.write("Internal Server Error");
+          response.end();
+        }
+      });
+    } else {
+      response.writeHead(405, {"Content-Type": "text/html"});
+      response.write(`Método ${request.method} no permitido!\r\n`);
+      response.end();
+    }
   }
-}
 
 
 const server = http.createServer(handleRequest);
